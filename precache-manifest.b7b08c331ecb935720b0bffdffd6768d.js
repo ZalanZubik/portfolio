f9fa0374c92b2dd7f3feaa618b94958612fd4ad0
Zalán Zubik <36884790+ZalanZubik@users.noreply.github.com>
2019-11-19 22:13:50 +0100

    Updates

---
+++ precache-manifest.b7b08c331ecb935720b0bffdffd6768d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3020bdf13b5bd2faf42b1664d3926bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a997938222d8c823d82",
+    "url": "/static/css/main.8ea56b97.chunk.css"
+  },
+  {
+    "revision": "66a3d5b99758fa99e297",
+    "url": "/static/js/2.4d936ec1.chunk.js"
+  },
+  {
+    "revision": "7a997938222d8c823d82",
+    "url": "/static/js/main.7bd47ac5.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "a1ceaf00e3a654070b4b50f969f14b66",
+    "url": "/static/media/ErrorImg.a1ceaf00.jpg"
+  },
+  {
+    "revision": "14acefb6e941ad5458e83e355894a49e",
+    "url": "/static/media/IntroImg.14acefb6.jpg"
+  }
+]);
